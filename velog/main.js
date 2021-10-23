bb679ce2325441ad2e240fa0d5563db732d39451
small-j <44703262+small-j@users.noreply.github.com>
2021-10-23 14:37:47 +0900

    feat: Add darkModeButton

--- velog/main.js
+++ velog/main.js
@@ -1,3 +1,4 @@
+import DarkModeButton from './component/darkmodeBotton.js';
 import Dropdown from './component/dropdown.js';
 import PostCardModal from './component/postCardModal.js';
 import {qs} from './utils.js';
@@ -15,6 +16,7 @@ class Main {
     init() {
         this.dropdown = new Dropdown();
         this.postCardModal = new PostCardModal();
+        this.darkModeBtn = new DarkModeButton();
     }
 
     setEvent() {

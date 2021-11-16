cd2ff425347ab41941ffaeda06db4c675bb06ac3
small-j <44703262+small-j@users.noreply.github.com>
2021-11-16 15:21:40 +0900

    refactor: console.log 삭제

--- github-profile-finder/src/App.js
+++ github-profile-finder/src/App.js
@@ -1,29 +1,19 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import Header from './components/Header';
-import Result from './components/Result';
-import Search from './components/Search';
-import styled from 'styled-components';
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Result from "./components/Result";
+import Search from "./components/Search";
+import styled from "styled-components";
 
 function App() {
-  const [userInfo, setUserInfo] = useState({data: null, status: 'idle'});
-
+  const [userInfo, setUserInfo] = useState({ data: null, status: "idle" });
   //아무 데이터도 받아오지 않은 상태 => idle
-
-  useEffect(() => {
-    console.log(userInfo);
-  }, [userInfo])
 
   return (
     <Root>
       <Header></Header>
-      <Search
-        setUserInfo={setUserInfo}
-      ></Search>
-      <Result
-        userInfo={userInfo}
-        setUserInfo={setUserInfo}
-      ></Result>
+      <Search setUserInfo={setUserInfo}></Search>
+      <Result userInfo={userInfo} setUserInfo={setUserInfo}></Result>
     </Root>
   );
 }
